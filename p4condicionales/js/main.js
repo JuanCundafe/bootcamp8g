bfd11bd88c71ed9b0a69a8bf76ff27524122a84c
tunombrejuanCundafe <juan.cundafe@crowdsense.app>
2020-05-27 18:39:29 -0500

    práctica 4

---
+++ p4condicionales/js/main.js
@@ -0,0 +1,30 @@
+var randomNumber = 10;
+
+if(randomNumber%2 == 0){
+    console.log("El número es par")
+}else{
+    console.log("El número no es par")
+}
+
+var length = 12;
+var width = 11;
+
+if(length%width == 0){
+    console.log("Es un cuadrado")
+}else{
+    console.log("Es un rectángulo")
+}
+
+var sideNumber = 5;
+
+if(sideNumber > 5){
+    console.log("Tu figura tiene muchos lados")
+}else if (sideNumber == 3){
+    console.log("Tu figura es un triángulo")
+}else if (sideNumber == 4){
+    console.log("Tu figura es un cuadrado")
+}else if (sideNumber == 5){
+    console.log("Tu figura es un pentágono")
+}else{
+    console.log("Tu figura esta rara")
+}
