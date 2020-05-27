046c7c5a25b3187c4400727cc4acbee4562bc238
tunombrejuanCundafe <juan.cundafe@crowdsense.app>
2020-05-26 19:24:54 -0500

    tarea javascript 1

---
+++ p3javascript/javascript/javascript.js
@@ -0,0 +1,38 @@
+var number1 = 5;
+var number2 = 6;
+
+var additionResult = number1 + number2
+
+var subtractionResult = number2 - number1
+
+var multiplicationResult = number2*number1
+
+var divisionResult = number2/number1
+
+var modulusResult = number2%number1
+
+var incrementResult = number2++
+
+var decrementResult = number1--
+
+console.log("number1 + number2", additionResult)
+
+console.log("number2 - number1", subtractionResult)
+
+console.log("number2*number1", multiplicationResult)
+
+console.log("number2/number1", divisionResult)
+
+console.log("number2%number1", modulusResult)
+
+console.log("number2++", incrementResult)
+
+console.log("number1--", decrementResult)
+
+
+
+
+
+
+
+
